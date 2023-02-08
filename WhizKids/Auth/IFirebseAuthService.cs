7a91122c32328dfae68bea5e664d774300060aee
Leah Plummer <leahplumm@gmail.com>
2023-02-08 15:26:32 -0600

    Added files necessary for Firebase authorization.

---
+++ WhizKids/Auth/IFirebseAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using WhizKids.Auth.Models;
+
+namespace WhizKids.Auth
+{
+    public interface IFirebaseAuthService
+    {
+        Task<FirebaseUser> Login(Credentials credentials);
+        Task<FirebaseUser> Register(Registration registration);
+    }
+}
